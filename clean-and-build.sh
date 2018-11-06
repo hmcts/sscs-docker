e4939a4c755db92b370d8fbac7336bbb214382a8
Chris Moreton <chris@netsensia.com>
2018-11-06 15:03:14 +0000

    Initial commit of clean and build script

---
+++ clean-and-build.sh
@@ -0,0 +1,36 @@
+# This script will tear down everything and rebuild
+export IDAM_KEY_DM_STORE=""
+export OAUTH2_CLIENT_CCD_ADMIN=""
+export IDAM_KEY_CCD_ADMIN=""
+
+echo "Please enter your hmcts.net email address: "
+read email
+
+./ccd compose down -v
+
+./ccd enable frontend backend dm-store
+./ccd compose pull
+
+DB_USERNAME=ccd DB_PASSWORD=password ./ccd compose up --build -d
+
+echo "Starting docker containers..."
+while [ `./ccd compose ps | grep starting | wc -l` != "0" ]
+do
+    echo "Waiting... for " `./ccd compose ps | grep starting | wc -l` " containers to start."
+    sleep 5
+done
+
+./bin/document-management-store-create-blob-store-container.sh
+
+DB_USERNAME=ccd DB_PASSWORD=password sh ./database/init-db.sh
+
+docker exec -ti compose_ccd-shared-database_1 psql -U postgres -c "CREATE DATABASE evidence"
+
+./bin/ccd-add-role.sh caseworker-sscs
+./bin/ccd-add-role.sh caseworker-sscs-systemupdate
+./bin/ccd-add-role.sh caseworker-sscs-anonymouscitizen
+./bin/ccd-add-role.sh caseworker-sscs-callagent
+
+./bin/idam-create-caseworker.sh caseworker-sscs,caseworker-sscs-systemupdate,caseworker-sscs-anonymouscitizen,caseworker-sscs-callagent $email
+
+
