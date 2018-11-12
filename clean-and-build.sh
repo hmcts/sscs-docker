428246258a8fd500bbb12e016582c797f2205e81
Chris Moreton <chris@netsensia.com>
2018-11-12 17:08:51 +0000

    Correct XLS variables in clean and build script

--- clean-and-build.sh
+++ clean-and-build.sh
@@ -8,7 +8,7 @@ source .env
 # Get the user's email address
 # Ignore if set in .env file
 #####################################################################################
-if [ -z $CCD_CASE_DEFINITION ]; then
+if [ -z $CCD_CASE_DEFINITION_XLS ]; then
   echo "================================================================================"
   echo "Please add the location of your CCD case definition spreadsheet to the .env file"
   echo "================================================================================"
@@ -84,7 +84,7 @@ done
 # Import the CCD definition files
 # Modify these commands to point to your local definition files
 #####################################################################################
-./bin/ccd-import-definition.sh $CCD_CASE_DEFINITION
+./bin/ccd-import-definition.sh $CCD_CASE_DEFINITION_XLS
 
 if [ -n $CCD_BULK_SCANNING_DEFINITION_XLS ]; then
   ./bin/ccd-import-definition.sh $CCD_BULK_SCANNING_DEFINITION_XLS
