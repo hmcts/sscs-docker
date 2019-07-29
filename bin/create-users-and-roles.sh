2e3ab9961ede40d7cda87659a8b57a30b22944c1
Chris Moreton <chris@netsensia.com>
2019-07-29 13:52:54 +0100

    Create system update user

--- bin/create-users-and-roles.sh
+++ bin/create-users-and-roles.sh
@@ -8,4 +8,6 @@ read EMAIL
 ./bin/idam-create-caseworker.sh ccd-import ccd.docker.default@hmcts.net Pa55word11 Default CCD_Docker
 ./bin/add-sscs-ccd-roles.sh
 ./bin/idam-create-caseworker.sh caseworker-sscs-systemupdate,caseworker-sscs,caseworker-sscs-clerk,caseworker,caseworker-sscs-superuser $EMAIL
+./bin/idam-create-caseworker.sh caseworker-sscs-systemupdate,caseworker system.update@hmcts.net
+
 ./bin/idam-create-caseworker.sh citizen sscs-citizen@hmcts.net
