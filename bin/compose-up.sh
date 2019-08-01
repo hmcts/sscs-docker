5aa1558465f618c8a7ef7371b3eda500d05c05e1
Chris Moreton <chris@netsensia.com>
2019-08-01 15:46:27 +0100

    Reinstate body of compose script

--- bin/compose-up.sh
+++ bin/compose-up.sh
@@ -4,7 +4,84 @@ set -e
 
 source .env
 
+command -v az >/dev/null 2>&1 || {
+    echo "=========================================================================================="
+    echo >&2 "Please install Azure CLI - instructions in README.md"
+    echo "=========================================================================================="
+    exit 1
+}
 
+command -v docker-compose >/dev/null 2>&1 || {
+    echo "=========================================================================================="
+    echo >&2 "Please install Docker Compose - instructions in README.md"
+    echo "=========================================================================================="
+    exit 1
+}
+
+command -v jq >/dev/null 2>&1 || {
+    echo "=========================================================================================="
+    echo >&2 "Please install jq - instructions in README.md"
+    echo "=========================================================================================="
+    exit 1
+}
+
+command -v java >/dev/null 2>&1 || {
+    echo "=========================================================================================="
+    echo >&2 "Please install java - instructions in README.md"
+    echo "=========================================================================================="
+    exit 1
+}
+
+if [ ! -f $CCD_CASE_DEFINITION_XLS ]; then
+  echo "=========================================================================================="
+  echo "CCD definition not found."
+  echo "Please check the CCD_CASE_DEFINITION_XLS value in  your .env file"
+  echo "Location is currently set to $CCD_CASE_DEFINITION_XLS"
+  echo "=========================================================================================="
+  exit
+fi
+
+if [ ! -f $CCD_BULK_SCAN_CASE_DEFINITION_XLS ]; then
+  echo "=========================================================================================="
+  echo "CCD Bulk Scan definition not found."
+  echo "Please check the CCD_BULK_SCAN_CASE_DEFINITION_XLS value in  your .env file"
+  echo "Location is currently set to $CCD_BULK_SCAN_CASE_DEFINITION_XLS"
+  echo "=========================================================================================="
+  exit
+fi
+
+if [ -z $HMCTS_EMAIL_ADDRESS ]; then
+  echo "=========================================================================================="
+  echo "Please add your HMCTS email address to the .env file"
+  echo "=========================================================================================="
+  exit
+fi
+
+source ./bin/set-environment-variables.sh
+
+./ccd login
+
+echo "Forcing re-creation of shared-db container to ensure SIDAM roles are cleared"
+
+./ccd compose down
+
+docker rm compose_shared-db_1 || true
+docker rm compose_ccd-shared-database_1 || true
+
+./ccd compose pull
+
+./ccd compose up -d
+
+./bin/document-management-store-create-blob-store-container.sh
+
+while [ `./ccd compose ps | grep starting | wc -l` != "0" ]
+do
+    echo "Waiting for " `./ccd compose ps | grep starting | wc -l` " containers to start."
+    sleep 5
+done
+
+echo
+echo "Everything looks ready."
 
 echo "Creating SIDAM services and roles. This will take around a minute..."
 
