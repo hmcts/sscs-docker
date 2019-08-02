3131700d75563b472e602b357fa1234a05c9290e
jackmaloney <jack.maloney@transformuk.com>
2019-08-02 10:44:32 +0100

    caseworker-sscs permission to system.update user

--- bin/create-users-and-roles.sh
+++ bin/create-users-and-roles.sh
@@ -6,6 +6,6 @@ source .env
 ./bin/idam-create-caseworker.sh ccd-import ccd.docker.default@hmcts.net Pa55word11 Default CCD_Docker
 ./bin/add-sscs-ccd-roles.sh
 ./bin/idam-create-caseworker.sh caseworker-sscs-systemupdate,caseworker-sscs,caseworker-sscs-clerk,caseworker,caseworker-sscs-superuser $HMCTS_EMAIL_ADDRESS
-./bin/idam-create-caseworker.sh caseworker-sscs-systemupdate,caseworker system.update@hmcts.net
+./bin/idam-create-caseworker.sh caseworker-sscs-systemupdate,caseworker-sscs,caseworker system.update@hmcts.net
 
 ./bin/idam-create-caseworker.sh citizen sscs-citizen@hmcts.net
