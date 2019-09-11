bd098dc038a026e02c309c4841b751cabea04945
Chris Moreton <chris@netsensia.com>
2019-09-11 14:40:09 +0100

    Remove HMCTS_EMAIL_ADDRESS

--- bin/create-users-and-roles.sh
+++ bin/create-users-and-roles.sh
@@ -5,7 +5,8 @@ source .env
 
 ./bin/idam-create-caseworker.sh ccd-import ccd.docker.default@hmcts.net Pa55word11 Default CCD_Docker
 ./bin/add-sscs-ccd-roles.sh
-./bin/idam-create-caseworker.sh caseworker-sscs-systemupdate,caseworker-sscs,caseworker-sscs-clerk,caseworker,caseworker-sscs-superuser $HMCTS_EMAIL_ADDRESS
+./bin/idam-create-caseworker.sh caseworker-sscs-systemupdate,caseworker-sscs,caseworker-sscs-clerk,caseworker,caseworker-sscs-superuser local.test@example.com
 ./bin/idam-create-caseworker.sh caseworker-sscs-systemupdate,caseworker-sscs,caseworker system.update@hmcts.net
+./bin/idam-create-caseworker.sh caseworker-sscs-systemupdate,caseworker-sscs,caseworker-sscs-clerk,caseworker,caseworker-sscs-superuser ron.smith@example.com
 
 ./bin/idam-create-caseworker.sh citizen sscs-citizen2@hmcts.net
