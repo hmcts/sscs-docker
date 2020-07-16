6ba1a679383ad69d5b5120430b633582abc4faad
jackmaloney <jack.maloney@transformuk.com>
2020-07-16 15:01:44 +0100

    Fix super user local

--- bin/create-users-and-roles.sh
+++ bin/create-users-and-roles.sh
@@ -5,7 +5,7 @@ source .env
 
 ./bin/idam-create-caseworker.sh ccd-import ccd.docker.default@hmcts.net Pa55word11 Default CCD_Docker
 ./bin/add-sscs-ccd-roles.sh
-./bin/idam-create-caseworker.sh caseworker-sscs-systemupdate,caseworker-sscs,caseworker-sscs-clerk,caseworker,caseworker-sscs-superuser local.test@example.com
+./bin/idam-create-caseworker.sh caseworker-sscs,caseworker,caseworker-sscs-superuser local.test@example.com
 ./bin/idam-create-caseworker.sh caseworker-sscs-systemupdate,caseworker-sscs,caseworker system.update@hmcts.net
 
 ./bin/idam-create-caseworker.sh citizen sscs-citizen2@hmcts.net
