629451a105e43b9d15421f01114a6467041df9f3
Del Attipoe <delali.attipoe@hmcts.net>
2022-02-02 16:00:23 +0000

    SSCS-9571 Add WA users and roles.

--- bin/create-simulator-users.sh
+++ bin/create-simulator-users.sh
@@ -4,8 +4,10 @@ source ./bin/set-environment-variables.sh
 source .env
 
 ./bin/simulator-create-caseworker.sh ccd-import ccd.docker.default@hmcts.net Pa55word11 Default CCD_Docker
-./bin/simulator-create-caseworker.sh caseworker-sscs,caseworker,caseworker-sscs-superuser local.test@example.com
+./bin/simulator-create-caseworker.sh caseworker-sscs,caseworker,caseworker-sscs-superuser,caseworker-wa-task-configuration local.test@example.com
 ./bin/simulator-create-caseworker.sh caseworker-sscs-systemupdate,caseworker-sscs,caseworker system.update@hmcts.net
+./bin/simulator-create-caseworker.sh caseworker-wa,caseworker-wa-task-configuration wa-system-user@fake.hmcts.net System01 WaUser WASystem
+
 
 ./bin/simulator-create-caseworker.sh caseworker,caseworker-sscs,caseworker-sscs-dwpresponsewriter dwpuser@example.com Pa55word11 DWP user
 ./bin/simulator-create-caseworker.sh caseworker,caseworker-sscs,caseworker-sscs-judge judge@example.com Pa55word11 Judge user
