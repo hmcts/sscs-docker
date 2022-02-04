b4f10c5b4f04fef35490ac727dd76898a69a724b
Del Attipoe <delali.attipoe@hmcts.net>
2022-02-04 20:55:04 +0000

    Temporary IA role changes due to xui whitelist.

--- bin/add-sscs-ccd-roles.sh
+++ bin/add-sscs-ccd-roles.sh
@@ -2,7 +2,7 @@
 
 source ./bin/set-environment-variables.sh
 
-roles=("caseworker-sscs" "citizen" "caseworker-sscs-systemupdate" "caseworker-sscs-anonymouscitizen" "caseworker-sscs-callagent" "caseworker-sscs-judge" "caseworker-sscs-clerk" "caseworker-sscs-dwpresponsewriter" "caseworker-sscs-registrar" "caseworker-sscs-superuser" "caseworker-sscs-teamleader"  "caseworker-sscs-panelmember" "caseworker-sscs-bulkscan" "caseworker-sscs-pcqextractor" "caseworker-wa-task-configuration")
+roles=("caseworker-sscs" "citizen" "caseworker-sscs-systemupdate" "caseworker-sscs-anonymouscitizen" "caseworker-sscs-callagent" "caseworker-sscs-judge" "caseworker-sscs-clerk" "caseworker-sscs-dwpresponsewriter" "caseworker-sscs-registrar" "caseworker-sscs-superuser" "caseworker-sscs-teamleader"  "caseworker-sscs-panelmember" "caseworker-sscs-bulkscan" "caseworker-sscs-pcqextractor" "caseworker-wa-task-configuration" "caseworker-ia-caseofficer") ##temp due to xui whitelist
 
 TRY_AGAIN_SECONDS=15
 ATTEMPTS=0
