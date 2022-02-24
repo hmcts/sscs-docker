f55f51de67a7cf4ced6cb5e01a71dab507c4f881
suetlaiHMCTS <suetlai.chan@hmcts.net>
2022-02-24 16:48:39 +0000

    Add role to sscs-case worker

--- bin/create-simulator-users.sh
+++ bin/create-simulator-users.sh
@@ -4,8 +4,8 @@ source ./bin/set-environment-variables.sh
 source .env
 
 ./bin/simulator-create-caseworker.sh ccd-import ccd.docker.default@hmcts.net Pa55word11 Default CCD_Docker
-./bin/simulator-create-caseworker.sh caseworker-sscs,caseworker,caseworker-sscs-superuser,caseworker-wa-task-configuration,caseworker-wa,caseworker-ia-caseofficer local.test@example.com ##ia temp again
-./bin/simulator-create-caseworker.sh caseworker-sscs-systemupdate,caseworker-sscs,caseworker,caseworker-wa-task-configuration,caseworker-wa system.update@hmcts.net
+./bin/simulator-create-caseworker.sh caseworker-sscs,caseworker,caseworker-sscs-superuser,caseworker-wa-task-configuration,caseworker-wa,caseworker-ia-caseofficer,caseworker-sscs-registrar local.test@example.com ##ia temp again
+./bin/simulator-create-caseworker.sh caseworker-sscs-systemupdate,caseworker-sscs,caseworker,caseworker-wa-task-configuration,caseworker-wa,caseworker-sscs-registrar system.update@hmcts.net
 ./bin/simulator-create-caseworker.sh caseworker-wa,caseworker-wa-task-configuration wa-system-user@fake.hmcts.net System01 WaUser WASystem
 
 
