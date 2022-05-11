e53b50318602070cf78f367e6e48cb71c2bff053
Mohammed Qureshi <mohammed.qureshi@HMCTS.NET>
2022-05-11 12:16:28 +0100

    Get paths before setting variable

--- bin/import-local-definition.sh
+++ bin/import-local-definition.sh
@@ -34,11 +34,11 @@ cd ../
 
 if [ ${ENV} == "prod" ]; then
   echo "Enviroment ${ENV}"
-  export CCD_CASE_DEFINITION_XLS=~/sscs-ccd-definitions/releases/CCD_SSCSDefinition_vdev_PROD.xlsx
+  export CCD_CASE_DEFINITION_XLS=${CCD_CASE_DEFINITION_XLS%/*}/CCD_SSCSDefinition_vdev_PROD.xlsx
   ./bin/create-xlsx.sh benefit dev prod
 else
   echo "Enviroment ${ENV}"
-  export CCD_CASE_DEFINITION_XLS=~/sscs-ccd-definitions/releases/CCD_SSCSDefinition_vdev_LOCAL.xlsx
+  export CCD_CASE_DEFINITION_XLS=${CCD_CASE_DEFINITION_XLS%/*}/CCD_SSCSDefinition_vdev_LOCAL.xlsx
   ./bin/create-xlsx.sh benefit dev local
 fi
 cd ../sscs-docker
